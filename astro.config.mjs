afa5e21eaf08793f66d38ba98081f444f5b597fa
yann <ymetier@openstudio.fr>
2023-02-16 00:56:48 +0100

    feat: workflow github build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+    site: 'https://anonymze.github.io',
+  base: '/ano_portfolio_2023',
     // output: 'server',
     integrations: [
         react(), tailwind(
